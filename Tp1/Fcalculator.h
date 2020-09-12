97079dea4d79c94af8949cd861d3a8c1da6fd2a8
Mauro <67985204+RuedaMauro@users.noreply.github.com>
2020-09-12 09:40:59 -0300

    Delete Fcalculator.h

--- Tp1/Fcalculator.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-float sumaDeOperadores(float, float);
-float restaDeOperadores(float, float);
-float divisionDeOperadores(int, float);
-int multiplicacionDeOperadores(int, int);
-int factorialPrimerNumero(int);
